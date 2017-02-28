1a302670b620d62115621d6f1bcc261dda8ca56a
sergs1207 <sergs1207@gmail.com>
2017-02-28 20:07:45 +0200

    Project structure is being created

---
+++ artifactid/src/main/java/mytempproject/App.java
@@ -0,0 +1,13 @@
+package mytempproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
